937e837f2d173eb92ee8444c6414a1dab52f1d8c
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-07-12 15:35:33 -0300

    Added home screen

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavBar()
+        setupToolbar()
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Note", for: indexPath)
+        return cell
+    }
+    
+    func setupNavBar() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Notes"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis.circle"), style: .plain, target: self, action: #selector(onMoreTapped))
+    }
+    
+    func setupToolbar() {
+        let notesCount = UILabel()
+        notesCount.text = "10 notes"
+        notesCount.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        notesCount.textColor = .secondaryLabel
+        
+        toolbarItems = [
+            UIBarButtonItem(systemItem: .flexibleSpace),
+            UIBarButtonItem(customView: notesCount),
+            UIBarButtonItem(systemItem: .flexibleSpace),
+            UIBarButtonItem(systemItem: .compose, primaryAction: UIAction(handler: onNewNoteTapped)),
+        ]
+        navigationController?.setToolbarHidden(false, animated: false)
+    }
+    
+    @objc func onMoreTapped() {
+        
+    }
+    
+    func onNewNoteTapped(_ action: UIAction) {
+        let emptyView = UIViewController()
+        navigationController?.pushViewController(emptyView, animated: true)
+    }
 }
 
+#Preview {
+    UIStoryboard(
+        name: "Main",
+        bundle: nil
+    )
+    .instantiateInitialViewController()!
+}
